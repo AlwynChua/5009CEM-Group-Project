8a5bcb6a72013c10c60ee20a0a5d86a0c92978eb
AlwynChua <112451566+AlwynChua@users.noreply.github.com>
2022-11-15 10:34:23 +0800

    Add files via upload

---
+++ 5009/src/oecd/Create_Table.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oecd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author User
+ */
+public class Create_Table {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        getConnection();
+        createTable();
+    }
+
+    public static Connection getConnection() throws Exception {
+
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost/5009_database";
+            String username = "root";
+            String password = "";
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
+            return conn;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+
+    public static void createTable() throws Exception {
+
+        try {
+            Connection conn = getConnection();
+            PreparedStatement create = conn.prepareStatement("CREATE TABLE IF NOT EXISTS companyinfo(id int NOT NULL AUTO_INCREMENT, companyName varchar(255), address varchar(255), city varchar(255), state varchar(255), zipcode int(100), service varchar(255), email varchar(255), password varchar(255), PRIMARY KEY(id))");
+            create.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("companyinfo table created.");
+        }
+        
+        try {
+            Connection conn = getConnection();
+            PreparedStatement create = conn.prepareStatement("CREATE TABLE IF NOT EXISTS testingTable(id int NOT NULL AUTO_INCREMENT, name varchar(255), police varchar(255), cincai varchar(255), state varchar(255), zipcode int(100), service varchar(255), email varchar(255), password varchar(255), PRIMARY KEY(id))");
+            create.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("companyinfo table created.");
+        }
+
+    }
+
+}
